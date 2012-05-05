8bfca96c6bc56e42462c85357bbf33f1fcec9a32
Yasuhisa Yoshida <syou6162@gmail.com>
2012-05-05 20:48:38 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject simple_shift_reduce_parsing "0.0.1"
+  :description "Dependency parser based on shift reduce algorithm"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]])
