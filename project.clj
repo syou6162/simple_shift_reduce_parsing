b0edf1ee81587e9a0f2a28d6f92ebc5674f878eb
Yasuhisa Yoshida <syou6162@gmail.com>
2012-08-08 22:20:51 +0900

    依存しているライブラリの変更

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Dependency parser based on shift reduce algorithm"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-		 [vijual "0.1.0-SNAPSHOT"]]
+		 [info.yasuhisay/vijual "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.2"]]
   :main simple_shift_reduce_parsing.core)
