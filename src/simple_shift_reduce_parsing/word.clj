7ad0fab9e2dd214c3435c68b3bd597d51444ccf3
Yasuhisa Yoshida <syou6162@gmail.com>
2012-09-26 05:09:47 -0700

    ちゃんとdependency parsingでの言葉で書くようにした

--- src/simple_shift_reduce_parsing/word.clj
+++ src/simple_shift_reduce_parsing/word.clj
@@ -1,5 +1,7 @@
 (ns simple_shift_reduce_parsing.word)
 
-(defstruct word :surface :pos-tag
-	   :original-idx :target-idx
-	   :children)
+(defstruct word :surface :pos-tag :idx :head :modifiers)
+
+;; (defstruct word :surface :pos-tag
+;; 	   :original-idx :target-idx
+;; 	   :children)
