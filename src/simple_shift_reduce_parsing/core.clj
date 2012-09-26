072221f706b9cbd349ec7dd3fdd9a4b9a1487def
Yasuhisa Yoshida <syou6162@gmail.com>
2012-09-26 05:18:08 -0700

    名前の変更に追従

--- src/simple_shift_reduce_parsing/core.clj
+++ src/simple_shift_reduce_parsing/core.clj
@@ -1,59 +1,17 @@
 (ns simple_shift_reduce_parsing.core
   (:use simple_shift_reduce_parsing.feature
 	simple_shift_reduce_parsing.word
-	simple_shift_reduce_parsing.action))
+	simple_shift_reduce_parsing.action
+        simple_shift_reduce_parsing.topological_sort))
 
-(use '[vijual :only (draw-tree)])
 (use '[clojure.string :only (split)])
 
-(defn extract-surface [sentence]
+(defn extract-surfaces [sentence]
   (if (not (vector? sentence))
-    (if (empty? (:children sentence))
-      [(keyword (:surface sentence))]
-      (apply conj [(:surface sentence)] (map extract-surface (:children sentence))))
-    (vec (map extract-surface sentence))))
-
-(let [sentence [{:surface :root, :pos-tag :root, :original-idx 0, :target-idx :root,
-		 :children [{:surface "sold", :pos-tag "VBD", :original-idx 7, :target-idx 0,
-			     :children [{:surface "maker", :pos-tag "NN", :original-idx 4, :target-idx 7,
-					 :children [{:surface "the", :pos-tag "DT", :original-idx 1, :target-idx 4, :children []}
-						    {:surface "luxury", :pos-tag "NN", :original-idx 2, :target-idx 4, :children []}
-						    {:surface "auto", :pos-tag "NN", :original-idx 3, :target-idx 4, :children []}]}
-					{:surface "year", :pos-tag "NN", :original-idx 6, :target-idx 7,
-					 :children [{:surface "last", :pos-tag "JJ", :original-idx 5, :target-idx 6, :children []}]}
-					{:surface "cars", :pos-tag "NNS", :original-idx 9, :target-idx 7,
-					 :children [{:surface "1,214", :pos-tag "CD", :original-idx 8, :target-idx 9, :children []}]}
-					{:surface "in", :pos-tag "IN", :original-idx 10, :target-idx 7,
-					 :children [{:surface "u.s.", :pos-tag "NNP", :original-idx 12, :target-idx 10,
-						     :children [{:surface "the", :pos-tag "DT", :original-idx 11, :target-idx 12, :children []}]}]}]}]}]]
-  (draw-tree (extract-surface sentence)))
-;;                             +------+
-;;                             | root |
-;;                             +--+---+
-;;                                |
-;;                                +
-;;                                |
-;;                             +--+---+
-;;                             | sold |
-;;                             +--+---+
-;;                                |
-;;             +------------------+---------+--------+
-;;             |                  |         |        |
-;;         +---+---+           +--+---+ +---+--+   +-+--+
-;;         | maker |           | year | | cars |   | in |
-;;         +---+---+           +--+---+ +---+--+   +-+--+
-;;             |                  |         |        |
-;;    +--------+---------+        +         +        +
-;;    |        |         |        |         |        |
-;; +--+--+ +---+----+ +--+---+ +--+---+ +---+---+ +--+---+
-;; | the | | luxury | | auto | | last | | 1,214 | | u.s. |
-;; +-----+ +--------+ +------+ +------+ +-------+ +--+---+
-;;                                                   |
-;;                                                   +
-;;                                                   |
-;;                                                +--+--+
-;;                                                | the |
-;;                                                +-----+
+    (if (empty? (:modifiers sentence))
+      [(:surface sentence)]
+      (apply conj [(:surface sentence)] (map extract-surfaces (:modifiers sentence))))
+    (vec (map extract-surfaces sentence))))
 
 (def filename "data/train.ulab")
 (def Root :root)
