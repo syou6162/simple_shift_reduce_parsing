66b9cfe1a6edab36bf3f40b2355f6fb9e9be85a7
Yasuhisa Yoshida <syou6162@gmail.com>
2012-09-26 09:23:45 +0900

    依存関係のバージョンを上げた

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject simple_shift_reduce_parsing "0.0.1"
   :description "Dependency parser based on shift reduce algorithm"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-		 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
 		 [info.yasuhisay/vijual "0.2.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.2"]]
+  :plugins [[lein-swank "1.4.4"]]
   :main simple_shift_reduce_parsing.core)
