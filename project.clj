f3406f5ac739a3bea56e63067b57d03ec156bf6d
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-08 15:41:28 +0900

    SVMは2クラスではなくて多クラス(shift, right, leftの3種類)なので、必要な分類器を変更

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Dependency parser based on shift reduce algorithm"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [fobos_clj "0.0.2"]
+                 [fobos_multiclass_clj "0.1.0-SNAPSHOT"]
 		 [info.yasuhisay/vijual "0.2.0-SNAPSHOT"]]
   :plugins [[lein-swank "1.4.4"]]
   :main simple_shift_reduce_parsing.core)
