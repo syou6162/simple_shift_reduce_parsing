d25323ce904882c72d4c682d28277531be1f442c
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-09 23:31:21 +0900

    マクロの名前がよくなかったので変更

--- src/simple_shift_reduce_parsing/feature.clj
+++ src/simple_shift_reduce_parsing/feature.clj
@@ -3,7 +3,7 @@
 (defstruct feature :type :str)
 (def feature-names (atom #{}))
 
-(defmacro deffeature-fn
+(defmacro def-feature-fn
   ([feature-name idx-op type]
      `(let [name# (defn ~feature-name [sentence# idx#]
                     (struct
@@ -12,25 +12,25 @@
                      (get-in sentence# [(~idx-op idx#) ~type])))]
         (swap! feature-names conj name#)))
   ([feature-name type]
-     `(deffeature-fn ~feature-name identity ~type)))
+     `(def-feature-fn ~feature-name identity ~type)))
 
-(deffeature-fn two-minus-word-feature (fn [x] (- x 3)) :surface)
-(deffeature-fn two-minus-pos-feature (fn [x] (- x 3)) :pos-tag)
+(def-feature-fn two-minus-word-feature (fn [x] (- x 3)) :surface)
+(def-feature-fn two-minus-pos-feature (fn [x] (- x 3)) :pos-tag)
 
-(deffeature-fn one-minus-word-feature (fn [x] (- x 2)) :surface)
-(deffeature-fn one-minus-pos-feature (fn [x] (- x 2)) :pos-tag)
+(def-feature-fn one-minus-word-feature (fn [x] (- x 2)) :surface)
+(def-feature-fn one-minus-pos-feature (fn [x] (- x 2)) :pos-tag)
 
-(deffeature-fn zero-minus-word-feature dec :surface)
-(deffeature-fn zero-minus-pos-feature dec :pos-tag)
+(def-feature-fn zero-minus-word-feature dec :surface)
+(def-feature-fn zero-minus-pos-feature dec :pos-tag)
 
-(deffeature-fn zero-plus-word-feature :surface)
-(deffeature-fn zero-plus-pos-feature :pos-tag)
+(def-feature-fn zero-plus-word-feature :surface)
+(def-feature-fn zero-plus-pos-feature :pos-tag)
 
-(deffeature-fn one-plus-word-feature inc :surface)
-(deffeature-fn one-plus-pos-feature inc :pos-tag)
+(def-feature-fn one-plus-word-feature inc :surface)
+(def-feature-fn one-plus-pos-feature inc :pos-tag)
 
-(deffeature-fn two-plus-word-feature (fn [x] (+ x 2)) :surface)
-(deffeature-fn two-plus-pos-feature (fn [x] (+ x 2)) :pos-tag)
+(def-feature-fn two-plus-word-feature (fn [x] (+ x 2)) :surface)
+(def-feature-fn two-plus-pos-feature (fn [x] (+ x 2)) :pos-tag)
 
 (defn get-fv [sentence idx]
   (->> (seq @feature-names)
