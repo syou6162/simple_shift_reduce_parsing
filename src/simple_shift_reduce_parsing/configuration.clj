7e6ff009a2d3f5047eaeba9ce4b054b4dbc3d879
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-25 18:51:04 -0700

    headからdependents、dependentからheadを見れるように変更(素性を作るときに必要となるので)。

--- src/simple_shift_reduce_parsing/configuration.clj
+++ src/simple_shift_reduce_parsing/configuration.clj
@@ -2,79 +2,16 @@
   (:refer-clojure :exclude [reduce])
   (:use simple_shift_reduce_parsing.word))
 
-(defrecord Configuration [stack input relation])
+(defrecord Configuration [stack input relations])
 
-(defn rcomp [& fs]
-  (apply comp (reverse fs)))
+(defn make-Configuration [input]
+  (Configuration. [] input {:head-to-modifiers {} :modifier-to-head {}}))
 
-(defn left [{old-stack :stack
-             old-input :input
-             old-relation :relation
-             :as configuration}]
-  (let [n (peek old-stack)
-        n' (first old-input)
-        stack (pop old-stack)
-        relation (assoc old-relation n' n)]
-    (-> configuration
-        (assoc :stack stack)
-        (assoc :relation relation))))
-
-(defn right [{old-stack :stack
-              old-input :input
-              old-relation :relation
-              :as configuration}]
-  (let [n (peek old-stack)
-        n' (first old-input)
-        stack (conj old-stack n')
-        input (vec (rest old-input))
-        relation (assoc old-relation n n')]
-    (-> configuration
-        (assoc :stack stack)
-        (assoc :input input)
-        (assoc :relation relation))))
-
-(defn reduce [{old-stack :stack
-               old-input :input
-               old-relation :relation
-               :as configuration}]
-  (let [stack (pop old-stack)]
-    (-> configuration
-        (assoc :stack stack))))
-
-(defn shift [{old-stack :stack
-              old-input :input
-              old-relation :relation
-              :as configuration}]
-  (let [n (first old-input)
-        stack (conj old-stack n)
-        input (vec (rest old-input))]
-    (-> configuration
-        (assoc :stack stack)
-        (assoc :input input))))
-
-(defn- get-gold-actions' [{stack :stack
-                           input :input
-                           relation :relation
-                           :as configuration}
-                          actions]
-  (if (empty? input)
-    actions
-    (let [n (peek stack)
-          n' (first input)]
-      (if (> (:head n') (:idx n'))
-        (if (= (:head n) (:idx n'))
-          (recur (left configuration)
-                 (conj actions :left))
-          (recur (shift configuration)
-                 (conj actions :shift)))
-        (if (= (:head n') (:idx n))
-          (recur (right configuration)
-                 (conj actions :right))
-          (recur (reduce configuration)
-                 (conj actions :reduce)))))))
-
-(defn get-gold-actions [{stack :stack
-                         input :input
-                         relation :relation
-                         :as configuration}]
-  (get-gold-actions' (shift configuration) [:shift]))
+(defn add-dependency-arc [^Configuration config head-node modifier-node]
+  (let [relations (:relations config)
+        new-relations (-> relations
+                          (update-in [:head-to-modifiers head-node]
+                                     (comp vec conj) modifier-node)
+                          (assoc-in [:modifier-to-head modifier-node]
+                                    head-node))]
+    (assoc config :relations new-relations)))
--- test/simple_shift_reduce_parsing/test/configuration.clj
+++ test/simple_shift_reduce_parsing/test/configuration.clj
@@ -1,15 +1,16 @@
 (ns simple_shift_reduce_parsing.test.configuration
-  (:require [simple_shift_reduce_parsing.configuration :as config])
+  (:use [simple_shift_reduce_parsing.configuration])
+  (:import [simple_shift_reduce_parsing.configuration Configuration])
   (:use [clojure.test]))
 
-(import '[simple_shift_reduce_parsing.configuration Configuration])
+(deftest make-Configuration-test
+  (are [x y] (= x y)
+       (make-Configuration [1 2 3])
+       (Configuration. [] [1 2 3] {:head-to-modifiers {}, :modifier-to-head {}})))
 
-(deftest get-gold-actions
-  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1, :modifiers []}
-                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-                  {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]]
-    (is (= (config/get-gold-actions (config/Configuration. [] sentence {}))
-           [:shift :shift :left :shift :reduce :right :right :reduce :right]))))
+(deftest add-relation-test
+  (is (= (-> (make-Configuration [1 2 3])
+             (add-dependency-arc 1 2)
+             (add-dependency-arc 1 3))
+         (Configuration. [] [1 2 3] {:modifier-to-head {3 1, 2 1},
+                                     :head-to-modifiers {1 [2 3]}}))))
