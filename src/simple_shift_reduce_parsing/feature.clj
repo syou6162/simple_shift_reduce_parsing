71b1b8157c1fc526763adf006540252d0a9093a7
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-25 19:29:26 -0700

    feature vectorを抽出する際の入力をconfigurationに変更し、親と子を素性に追加しやすくした

--- src/simple_shift_reduce_parsing/feature.clj
+++ src/simple_shift_reduce_parsing/feature.clj
@@ -1,74 +1,133 @@
 (ns simple_shift_reduce_parsing.feature
+  (:require [simple_shift_reduce_parsing.configuration :as config])
+  (:require [simple_shift_reduce_parsing.action :as action])
   (:use clojure.set))
+
+(import '[simple_shift_reduce_parsing.configuration Configuration])
 
 (defstruct feature :type :str)
 (def feature-names (atom #{}))
-(def children-feature-names (atom #{}))
+
+(defn get-stack-idx [stack idx]
+  (let [n (count stack)]
+    (- n (- idx))))
 
 (defmacro def-feature-fn
-  ([feature-name idx-op type]
-     `(let [name# (defn ~feature-name [sentence# idx#]
+  ([feature-name idx type]
+     `(let [name# (defn ~feature-name [configuration#]
                     (struct
                      feature
                      '~feature-name
-                     (get-in sentence# [(~idx-op idx#) ~type])))]
+                     (if (neg? ~idx)
+                       (let [stack# (get configuration# :stack)]
+                         (get-in stack# [(get-stack-idx stack# ~idx) ~type]))
+                       (get-in configuration# [:input ~idx ~type]))))]
         (swap! feature-names conj name#))))
 
-(defmacro def-children-feature-fn
-  ([feature-name idx-op type]
-     `(let [name# (defn ~feature-name [sentence# idx#]
-                    (let [head# (get-in sentence# [(~idx-op idx#)])]
-                      (if (nil? head#)
-                        '()
-                        (map
-                         (fn [word#]
-                          (struct
-                           feature
-                           '~feature-name
-                           (word# ~type)))
-                         (head# :modifiers)))))]
-        (swap! children-feature-names conj name#))))
+; (def-feature-fn two-minus-word-feature -3 :surface)
+; (def-feature-fn two-minus-pos-feature -3 :pos-tag)
 
-(def-feature-fn two-minus-word-feature (fn [x] (- x 3)) :surface)
-(def-feature-fn two-minus-pos-feature (fn [x] (- x 3)) :pos-tag)
+; (def-feature-fn one-minus-word-feature -2 :surface)
+; (def-feature-fn one-minus-pos-feature -2 :pos-tag)
 
-(def-children-feature-fn two-minus-children-word-features (fn [x] (- x 3)) :surface)
-(def-children-feature-fn two-minus-children-pos-features (fn [x] (- x 3)) :pos-tag)
+(def-feature-fn zero-minus-word-feature -1 :surface)
+(def-feature-fn zero-minus-pos-feature -1 :pos-tag)
 
-(def-feature-fn one-minus-word-feature (fn [x] (- x 2)) :surface)
-(def-feature-fn one-minus-pos-feature (fn [x] (- x 2)) :pos-tag)
+(def-feature-fn zero-plus-word-feature 0 :surface)
+(def-feature-fn zero-plus-pos-feature 0 :pos-tag)
 
-(def-children-feature-fn one-minus-children-word-features (fn [x] (- x 2)) :surface)
-(def-children-feature-fn one-minus-children-pos-features (fn [x] (- x 2)) :pos-tag)
+; (def-feature-fn one-plus-word-feature 1 :surface)
+(def-feature-fn one-plus-pos-feature 1 :pos-tag)
 
-(def-feature-fn zero-minus-word-feature dec :surface)
-(def-feature-fn zero-minus-pos-feature dec :pos-tag)
+; (def-feature-fn two-plus-word-feature 2 :surface)
+(def-feature-fn two-plus-pos-feature 2 :pos-tag)
 
-(def-children-feature-fn zero-minus-children-word-features dec :surface)
-(def-children-feature-fn zero-minus-children-pos-features dec :pos-tag)
+; (def-feature-fn three-plus-word-feature 3 :surface)
+(def-feature-fn three-plus-pos-feature 3 :pos-tag)
 
-(def-feature-fn zero-plus-word-feature identity :surface)
-(def-feature-fn zero-plus-pos-feature identity :pos-tag)
+;; NL.POS feature
+(swap! feature-names conj
+       (defn leftmost-dependent-of-input-pos-feature [{stack :stack
+                                                       input :input
+                                                       relations :relations
+                                                       :as config}]
+         (let [n (first input) ; next input token
+               nl (first (get (:head-to-modifiers relations) n))] ; leftmost dependent of n
+           (struct feature
+                   'leftmost-dependent-of-input-pos-feature
+                   (:pos-tag nl)))))
 
-(def-children-feature-fn zero-plus-children-word-features identity :surface)
-(def-children-feature-fn zero-plus-children-pos-features identity :pos-tag)
+;; TH.POS feature
+(swap! feature-names conj
+       (defn head-of-pos-feature [{stack :stack
+                                   input :input
+                                   relations :relations
+                                   :as config}]
+         (let [t (peek stack) ; top of the stack
+               th (get-in relations [:modifier-to-head t])] ; head of t
+           (struct feature
+                   'head-of-pos-feature
+                   (:pos-tag th)))))
 
-(def-feature-fn one-plus-word-feature inc :surface)
-(def-feature-fn one-plus-pos-feature inc :pos-tag)
+(swap! feature-names conj
+       (defn leftmost-dependent-of-stack-pos-feature [{stack :stack
+                                                       input :input
+                                                       relations :relations
+                                                       :as config}]
+         (let [t (peek stack) ; top of the stack
+               tl (first (get-in relations [:head-to-modifiers t]))]
+           (struct feature
+                   'leftmost-dependent-of-stack-pos-feature
+                   (:pos-tag tl)))))
 
-(def-children-feature-fn one-plus-children-word-features inc :surface)
-(def-children-feature-fn one-plus-children-pos-features inc :pos-tag)
+(swap! feature-names conj
+       (defn rightmost-dependent-of-stack-pos-feature [{stack :stack
+                                                        input :input
+                                                        relations :relations
+                                                        :as config}]
+         (let [t (peek stack) ; top of the stack
+               tr (peek (get-in relations [:head-to-modifiers t]))]
+           (struct feature
+                   'rightmost-dependent-of-stack-pos-feature
+                   (:pos-tag tr)))))
 
-(def-feature-fn two-plus-word-feature (fn [x] (+ x 2)) :surface)
-(def-feature-fn two-plus-pos-feature (fn [x] (+ x 2)) :pos-tag)
+(defn feature-bigram [f1 f2]
+  (let [type1 (:type f1)
+        str1 (:str f1)
+        type2 (:type f2)
+        str2 (:str f2)]
+    (struct feature
+            (symbol (str type1 "-and-" type2))
+            (str str1 "-and-" str2))))
 
-(def-children-feature-fn two-plus-children-word-features (fn [x] (+ x 2)) :surface)
-(def-children-feature-fn two-plus-children-pos-features (fn [x] (+ x 2)) :pos-tag)
+(defn combinational-features [raw-fv]
+  (let [n (count raw-fv)
+        fv-bigram (for [i (range n)
+                        j (range n)
+                        :when (< i j)]
+                    (feature-bigram (nth raw-fv i) (nth raw-fv j)))]
+    (vec (concat raw-fv fv-bigram))))
 
-(defn get-fv [sentence idx]
-  (->> (seq (union @feature-names @children-feature-names))
-       (map (fn [feature-fn] (feature-fn sentence idx)))
-       (flatten)
-       (filter (fn [fv] (not (nil? (:str fv)))))
-       (map #(vector % 1))
-       (vec)))
+(defn get-fv [configuration]
+  (let [raw-fv (->> (seq @feature-names)
+                    (map (fn [feature-fn] (feature-fn configuration)))
+                    (flatten)
+                    (filter (fn [fv] (not (nil? (:str fv))))))]
+    (->> (combinational-features raw-fv)
+         (map #(vector % 1))
+         (vec))))
+
+(defn generate-gold [sentence]
+  (let [init-config (config/make-Configuration sentence)
+        actions (action/get-gold-actions init-config)]
+    (loop [config init-config
+           actions-idx 0
+           result []]
+      (if (= actions-idx (count actions))
+        result
+        (let [next-action (nth actions actions-idx)
+              next-config ((action/action-mapping next-action) config)]
+          (recur
+           next-config
+           (inc actions-idx)
+           (conj result [next-action (get-fv config)])))))))
--- test/simple_shift_reduce_parsing/test/feature.clj
+++ test/simple_shift_reduce_parsing/test/feature.clj
@@ -1,44 +1,79 @@
 (ns simple_shift_reduce_parsing.test.feature
-  (:use [clojure.test])
-  (:use [simple_shift_reduce_parsing.feature]))
+  (:require [simple_shift_reduce_parsing.configuration :as config])
+  (:require [simple_shift_reduce_parsing.action :as action])
+  (:use [simple_shift_reduce_parsing.feature])
+  (:use [clojure.test]))
+
+(import '[simple_shift_reduce_parsing.configuration Configuration])
 
 (deftest test-feature
- (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-                 {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0,
-                  :modifiers [{:surface "haag", :pos-tag "NNP", :idx 2, :head 3,
-                               :modifiers [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}]}
-                              {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}]}
-                 {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]]
-   (are [x y] (= x y)
-        (one-minus-word-feature sentence 2)
-        {:type 'one-minus-word-feature, :str :root}
+  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                  {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+        configuration ((comp action/shift) (config/Configuration. [] sentence {}))]
+    (are [x y] (= x y)
+         (zero-minus-word-feature configuration)
+         {:type 'zero-minus-word-feature, :str :root}
 
-        (one-minus-pos-feature sentence 2)
-        {:type 'one-minus-pos-feature, :str :root}
-        
-        (zero-minus-word-feature sentence 2)
-        {:type 'zero-minus-word-feature, :str "plays"}
-        
-        (zero-minus-pos-feature sentence 2)
-        {:type 'zero-minus-pos-feature, :str "VBZ"}
+         (zero-minus-pos-feature configuration)
+         {:type 'zero-minus-pos-feature, :str :root}
 
-        (zero-minus-children-word-features sentence 2)
-        (list {:type 'zero-minus-children-word-features, :str "haag"} {:type 'zero-minus-children-word-features, :str "elianti"})
+         (zero-plus-word-feature configuration)
+         {:type 'zero-plus-word-feature, :str "ms."}
 
-        (zero-minus-children-pos-features sentence 2)
-        (list {:type 'zero-minus-children-pos-features, :str "NNP"} {:type 'zero-minus-children-pos-features, :str "NNP"})
-        
-        (zero-plus-word-feature sentence 2)
-        {:type 'zero-plus-word-feature, :str "."}
-        
-        (zero-plus-pos-feature sentence 2)
-        {:type 'zero-plus-pos-feature, :str "."}
+         (zero-plus-pos-feature configuration)
+         {:type 'zero-plus-pos-feature, :str "NNP"})))
 
-        (one-plus-word-feature sentence 2)
-        {:type 'one-plus-word-feature, :str nil}
-        
-        (one-plus-pos-feature sentence 2)
-        {:type 'one-plus-pos-feature, :str nil})
-   
-   (is (every? #(not (nil? %)) (map (comp :str first) (get-fv sentence 1))))
-   (is (every? (partial = 1) (map (comp second) (get-fv sentence 1))))))
+(deftest leftmost-dependent-of-input-pos-feature-test
+  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1, :modifiers []}
+                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
+                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
+                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
+                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
+                  {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]
+        init-config ((comp action/left action/shift action/shift)
+                     (config/make-Configuration sentence))]
+    (is (= (leftmost-dependent-of-input-pos-feature init-config)
+           {:type 'leftmost-dependent-of-input-pos-feature, :str "NNP"}))))
+
+(deftest head-of-pos-feature-test
+  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                  {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+        init-config ((comp action/right action/reduce action/shift
+                           action/left action/shift action/shift)
+                     (config/Configuration. [] sentence {}))]
+    (is (= (head-of-pos-feature init-config)
+           {:type 'head-of-pos-feature, :str :root}))))
+
+(deftest rightmost-dependent-of-stack-pos-feature-test
+  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                  {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+        config ((comp action/reduce action/right action/right
+                      action/reduce action/shift action/left
+                      action/shift action/shift)
+                (config/make-Configuration sentence))]
+    (is (= (rightmost-dependent-of-stack-pos-feature config)
+           {:type 'rightmost-dependent-of-stack-pos-feature, :str "NNP"}))))
+
+(deftest feature-bigram-test
+  (is (= (feature-bigram (struct feature 'hoge :fuga)
+                         (struct feature 'hoge :fuga))
+         {:type 'hoge-and-hoge, :str ":fuga-and-:fuga"})))
+
+(deftest combinational-features-test
+  (is (= (combinational-features [(struct feature 'hoge1 :hoge1)
+                                  (struct feature 'hoge2 :hoge2)])
+         [{:type 'hoge1, :str :hoge1}
+          {:type 'hoge2, :str :hoge2}
+          {:type 'hoge1-and-hoge2, :str ":hoge1-and-:hoge2"}])))
