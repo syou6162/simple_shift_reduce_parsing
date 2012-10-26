ee7a18d1b2f74a51e6809c70e0b957c5cf1a265b
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-25 18:54:18 -0700

    CoNLL Xが持っている情報と合わせた。modifiersの情報は陽には持たせないように変更

--- src/simple_shift_reduce_parsing/word.clj
+++ src/simple_shift_reduce_parsing/word.clj
@@ -1,7 +1,3 @@
 (ns simple_shift_reduce_parsing.word)
 
-(defstruct word :surface :pos-tag :idx :head :modifiers)
-
-;; (defstruct word :surface :pos-tag
-;; 	   :original-idx :target-idx
-;; 	   :children)
+(defstruct word :surface :pos-tag :idx :head)
