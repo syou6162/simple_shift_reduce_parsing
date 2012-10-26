d7b97a4cbeea12e4314498d6314157e8ff1bd0ee
Yasuhisa Yoshida <syou6162@gmail.com>
2012-10-25 19:54:20 -0700

    Yamada and Matsumoto 2003のactionが3つのものからNivre 2004のactionが4つあるものに変更

--- src/simple_shift_reduce_parsing/action.clj
+++ src/simple_shift_reduce_parsing/action.clj
@@ -1,76 +1,105 @@
 (ns simple_shift_reduce_parsing.action
-  (:use simple_shift_reduce_parsing.feature))
+  (:use simple_shift_reduce_parsing.configuration)
+  (:refer-clojure :exclude [reduce]))
 
-(defn safe-index [idx sentence-length]
-  (if (> sentence-length idx) idx 1))
+(defn shift [{old-stack :stack
+              old-input :input
+              :as configuration}]
+  "Shift operation for configuration.
+   <S, n | I, A> => <n | S, I , A>"
+  (assert (not (empty? old-input)))
+  (let [n (first old-input)
+        stack (conj old-stack n)
+        input (vec (rest old-input))]
+    (-> configuration
+        (assoc :stack stack)
+        (assoc :input input))))
 
-(defn shift [[idx sentence]]
-  [(safe-index (inc idx) (count sentence))
-   sentence])
+(defn reducable? [config]
+  (or (not (empty? (:stack config)))
+      (contains? (-> config :relations :modifier-to-head)
+                 (peek (:stack config)))))
 
-;; [[a] [b] [c] [d]]でbとcの間でright => [[a] [b [c]] [d]]
-(defn right [[idx sentence]]
-  (let [[lhs rhs] (map vec (split-at idx sentence))
-	left-word (last lhs)
-	right-word (assoc (first rhs) :head (:idx left-word))]
-    [(safe-index idx (dec (count sentence)))
-     (vec (concat (vec (butlast lhs))
-		  (concat [(update-in left-word
-				      [:modifiers]
-				      conj
-				      right-word)]
-			  (vec (drop 1 rhs)))))]))
+(defn reduce [{old-stack :stack
+               old-input :input
+               :as configuration}]
+  "Reduce operation for configuration
+   <n | S, I , A> => <S, I, A>"
+  (if (reducable? configuration)
+    (let [stack (pop old-stack)]
+      (-> configuration
+          (assoc :stack stack)))
+    (shift configuration)))
 
-;; [[a] [b] [c] [d]]でbとcの間でleft => [[a] [c [b]] [d]]
-(defn left [[idx sentence]]
-  (let [[lhs rhs] (map vec (split-at idx sentence))
-	right-word (first rhs)
-        left-word (assoc (last lhs) :head (:idx right-word))]
-    [(safe-index idx (dec (count sentence)))
-     (vec (concat (vec (butlast lhs))
-		  (concat [(update-in right-word
-				      [:modifiers]
-				      #(vec (concat [%2] %1))
-				      left-word)]
-			  (vec (drop 1 rhs)))))]))
+(defn leftable? [config]
+  (and (not (empty? (:stack config)))
+       (not (contains? (-> config :relations :modifier-to-head)
+                       (peek (:stack config))))))
 
-(def actions #{right left shift})
+(defn left [{old-stack :stack
+             old-input :input
+             old-relations :relations
+             :as config}]
+  "Left operation for configuration
+   <n | S, n' | I, A> => <S, n' | I, A \\cup {(n', n)}>"
+  (if (leftable? config)
+    (let [n (peek old-stack) ; modifier
+          n' (first old-input) ; head
+          stack (pop old-stack)
+          relations (:relations (add-dependency-arc config n' n))]
+      (assert (not (empty? old-input)))
+      (-> config
+          (assoc :stack stack)
+          (assoc :relations relations)))
+    (reduce config)))
 
-(def action-mapping {:right right
-		     :left left
+(defn rightable? [config]
+  (and (not (empty? (:stack config)))
+       (not (contains? (-> config :relations :modifier-to-head)
+                       (first (-> config :input))))))
+
+(defn right [{old-stack :stack
+              old-input :input
+              old-relation :relation
+              :as config}]
+  (if (rightable? config)
+    (let [n (peek old-stack) ; head
+          n' (first old-input) ; modifier
+          stack (conj old-stack n')
+          input (vec (rest old-input))
+          relations (:relations (add-dependency-arc config n n'))]
+      (assert (not (empty? old-input)))
+      (-> config
+          (assoc :stack stack)
+          (assoc :input input)
+          (assoc :relations relations)))
+    (reduce config)))
+
+(def action-mapping {:left left
+                     :right right
+                     :reduce reduce
 		     :shift shift})
 
-(defn generate-gold-seq-of-actions' [[action idx sentence]]
-  (let [[idx sentence] ((get action-mapping action identity) [idx sentence])
-	[lhs rhs] (map vec (split-at idx sentence))
-	left-word (last lhs)
-	right-word (first rhs)
-	next-action (cond (= 1 (count sentence)) nil
-			  (and
-			   ;; 係り先の単語に係る単語が左のコンテキストに一つもいない
-			   (reduce (fn [cum w] (and cum (not (= (:head w) (:idx left-word)))))
-				   true (butlast lhs))
-			   (= (:head left-word) (:idx right-word)))
-			  :left
-			  (and
-			   ;; 係り先の単語に係る単語が右のコンテキストに一つもいない
-			   (reduce (fn [cum w] (and cum (not (= (:head w) (:idx right-word)))))
-				   true (rest rhs))
-			   (= (:head right-word) (:idx left-word)))
-			  :right
-			  :else :shift)]
-    [next-action idx sentence]))
+(defn- get-gold-actions' [{stack :stack
+                           input :input
+                           relation :relation
+                           :as configuration}
+                          actions]
+  (if (empty? input)
+    actions
+    (let [n (peek stack)
+          n' (first input)]
+      (if (> (:head n') (:idx n'))
+        (if (= (:head n) (:idx n'))
+          (recur (left configuration)
+                 (conj actions :left))
+          (recur (shift configuration)
+                 (conj actions :shift)))
+        (if (= (:head n') (:idx n))
+          (recur (right configuration)
+                 (conj actions :right))
+          (recur (reduce configuration)
+                 (conj actions :reduce)))))))
 
-(defn generate-gold-seq-of-actions [sentence]
-  (->> (iterate generate-gold-seq-of-actions' [nil 1 sentence])
-       (rest) ;; 初期は捨てる
-       (take-while (fn [[action _ _]] (not (nil? action))))
-       (map (fn [[action idx sentence]] {:action action
-					 :index idx
-					 :sentence sentence}))))
-
-(defn generate-gold [sentence]
-  (map (fn [chunk]
-         (vector (:action chunk)
-                 (get-fv (:sentence chunk) (:index chunk))))
-       (generate-gold-seq-of-actions sentence)))
+(defn get-gold-actions [config]
+  (get-gold-actions' (shift config) [:shift]))
--- test/simple_shift_reduce_parsing/test/action.clj
+++ test/simple_shift_reduce_parsing/test/action.clj
@@ -1,89 +1,83 @@
 (ns simple_shift_reduce_parsing.test.action
-  (:use [simple_shift_reduce_parsing.action])
+  (:require [simple_shift_reduce_parsing.configuration :as config])
+  (:require [simple_shift_reduce_parsing.action :as action])
   (:use [clojure.test]))
 
-(def sentence
-  [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-   {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-   {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-   {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-   {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-   {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}])
+(import '[simple_shift_reduce_parsing.configuration Configuration])
 
-(deftest test-shift
-  (is (= (shift [2 sentence])
-           [3 [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-               {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-               {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-               {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-               {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-               {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]]))
-  (is (= (shift [5 sentence])
-         [1 [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-             {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-             {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-             {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-             {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-             {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]])))
+(def sentence [{:surface :root, :pos-tag :root, :idx 0, :head -1}
+               {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+               {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+               {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+               {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+               {:surface ".", :pos-tag ".", :idx 5, :head 3}])
 
-(deftest test-right
-  (is (= (right [1 sentence])
-         [1 [{:surface :root, :pos-tag :root, :idx 0, :head :root,
-              :modifiers [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 0, :modifiers []}]}
-             {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-             {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-             {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-             {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]]))
+(deftest shift-test
+  (are [x y] (= x y)
+       (action/shift (config/make-Configuration sentence))
+       (config/Configuration. [{:surface :root, :pos-tag :root, :idx 0, :head -1}]
+                              [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                               {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                               {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                               {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                               {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+                              {:head-to-modifiers {}
+                               :modifier-to-head {}})))
 
-  (is (= (right [5 sentence])
-         [1 [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-             {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-             {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-             {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-             {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3,
-              :modifiers [{:surface ".", :pos-tag ".", :idx 5, :head 4, :modifiers []}]}]])))
+(deftest reducable?-test
+  (let [c (config/Configuration. [{:surface :root, :pos-tag :root, :idx 0, :head -1}]
+                                 [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                                  {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+                                 {:modifier-to-head
+                                  {{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                                   {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}}
+                                  :head-to-modifiers {}})]
+    (are [x y] (= x y)
+         (action/reducable? c) true)))
 
-(deftest test-left
-  (is (= (left [1 sentence])
-         [1 [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2,
-              :modifiers [{:surface :root, :pos-tag :root, :idx 0, :head 1, :modifiers []}]}
-             {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-             {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-             {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-             {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]]))
+(deftest reduce-test
+  (let [c (config/Configuration. [{:surface :root, :pos-tag :root, :idx 0, :head -1}]
+                                 [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                  {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                                  {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                                  {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                                  {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+                                 {:modifier-to-head
+                                  {{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                                   {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}}
+                                  :head-to-modifiers
+                                  {{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                   [{:surface :root, :pos-tag :root, :idx 0, :head -1}]}})]
+    (are [x y] (= x y)
+         ;; reducable case
+         (action/reduce c)
+         (config/Configuration. []
+                                [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                 {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                                 {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                                 {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                                 {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+                                {:modifier-to-head
+                                 {{:surface :root, :pos-tag :root, :idx 0, :head -1}
+                                  {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}}
+                                 :head-to-modifiers
+                                 {{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                  [{:surface :root, :pos-tag :root, :idx 0, :head -1}]}})
+         
+         ;; unreducable case => instead of reduce, do shift operation
+         (action/reduce (config/Configuration. [] sentence {}))
+         (config/Configuration. [{:surface :root, :pos-tag :root, :idx 0, :head -1}]
+                                [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2}
+                                 {:surface "haag", :pos-tag "NNP", :idx 2, :head 3}
+                                 {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0}
+                                 {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3}
+                                 {:surface ".", :pos-tag ".", :idx 5, :head 3}]
+                                {}))))
 
-  (is (= (left [5 sentence])
-         [1 [{:surface :root, :pos-tag :root, :idx 0, :head :root, :modifiers []}
-             {:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}
-             {:surface "haag", :pos-tag "NNP", :idx 2, :head 3, :modifiers []}
-             {:surface "plays", :pos-tag "VBZ", :idx 3, :head 0, :modifiers []}
-             {:surface ".", :pos-tag ".", :idx 5, :head 3,
-              :modifiers [{:surface "elianti", :pos-tag "NNP", :idx 4, :head 5, :modifiers []}]}]])))
-
-(deftest test-actions
-  (is (= ((comp right right right left left shift) [1 sentence])
-	 [1 [{:surface :root, :pos-tag :root, :idx 0, :head :root,
-	      :modifiers [{:surface "plays", :pos-tag "VBZ", :idx 3, :head 0,
-                           :modifiers [{:surface "haag", :pos-tag "NNP", :idx 2, :head 3,
-                                        :modifiers [{:surface "ms.", :pos-tag "NNP", :idx 1, :head 2, :modifiers []}]}
-                                       {:surface "elianti", :pos-tag "NNP", :idx 4, :head 3, :modifiers []}
-                                       {:surface ".", :pos-tag ".", :idx 5, :head 3, :modifiers []}]}]}]])))
-
-(deftest test-generate-gold
-  (let [sentence [{:surface :root, :pos-tag :root, :idx 0, :head nil :modifiers []}
-                  {:surface "the", :pos-tag "DT", :idx 1, :head 4 :modifiers []}
-                  {:surface "luxury", :pos-tag "NN", :idx 2, :head 4 :modifiers []}
-                  {:surface "auto", :pos-tag "NN", :idx 3, :head 4 :modifiers []}
-                  {:surface "maker", :pos-tag "NN", :idx 4, :head 7 :modifiers []}
-                  {:surface "last", :pos-tag "JJ", :idx 5, :head 6 :modifiers []}
-                  {:surface "year", :pos-tag "NN", :idx 6, :head 7 :modifiers []}
-                  {:surface "sold", :pos-tag "VBD", :idx 7, :head 0 :modifiers []}
-                  {:surface "1,214", :pos-tag "CD", :idx 8, :head 9 :modifiers []}
-                  {:surface "cars", :pos-tag "NNS", :idx 9, :head 7 :modifiers []}
-                  {:surface "in", :pos-tag "IN", :idx 10, :head 7 :modifiers []}
-                  {:surface "the", :pos-tag "DT", :idx 11, :head 12 :modifiers []}
-                  {:surface "u.s.", :pos-tag "NNP", :idx 12, :head 10 :modifiers []}]]
-    (is (= '(:shift :shift :shift :left :shift :left :left :shift
-                    :left :shift :shift :left :shift :shift :left
-                    :shift :right :shift :right :shift :left :left :right :right)
-           (map :action (generate-gold-seq-of-actions sentence))))))
+(deftest get-gold-actions-test
+  (is (= (action/get-gold-actions
+          (config/make-Configuration sentence))
+         [:shift :shift :left :shift :reduce :right :right :reduce :right])))
