c2e4cfb395692fa93a2c384498913032063a30ac
Yasuhisa Yoshida <syou6162@gmail.com>
2013-09-05 08:36:15 +0900

    ライブラリのバージョンを上げたり、不必要な依存を減らした

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject simple-shift-reduce-parsing "0.0.2"
   :description "Dependency parser based on shift reduce algorithm"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [dorothy "0.0.3"]
-                 [info.yasuhisay/clj-utils "0.1.0-SNAPSHOT"]
+                 [info.yasuhisay/clj-utils "0.1.1"]
                  [de.bwaldvogel/liblinear "1.92"]
                  [info.yasuhisay/liblinear "0.0.1"]
                  [log4j/log4j "1.2.16"
@@ -12,6 +12,5 @@
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.7"]]
-  :plugins [[lein-swank "1.4.4"]]
   :jvm-opts ["-Xmx8g" "-server" "-Dfile.encoding=UTF-8"]
   :main simple_shift_reduce_parsing.core)
