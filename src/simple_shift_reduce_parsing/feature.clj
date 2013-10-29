1d42fde85ce0f43ed0c7e4133db425e34eac07b8
Yasuhisa Yoshida <syou6162@gmail.com>
2013-10-29 01:55:10 -0700

    generate-goldを削除、get-fvはstringのvectorを返すように変更

--- src/simple_shift_reduce_parsing/feature.clj
+++ src/simple_shift_reduce_parsing/feature.clj
@@ -310,23 +310,4 @@
                                    (feature-fn configuration))))
                     (filter (fn [fv] (not (nil? (:str fv))))))]
     (->> raw-fv
-         (map (fn [f] (str (:type f) "-and-" (:str f))))
-         (map feature-to-id)
-         (map #(vector % 1))
-         (vec))))
-
-(defn generate-gold [sentence]
-  (let [^Configuration init-config (config/make-Configuration sentence)
-        actions (action/get-gold-actions init-config)]
-    (loop [^Configuration config init-config
-           actions-idx 0
-           result []]
-      (if (= actions-idx (count actions))
-        result
-        (let [current-action (nth actions actions-idx)
-              ^Configuration next-config ((action/action-mapping current-action) config)]
-          (recur
-           next-config
-           (inc actions-idx)
-           (conj result [(action/action2id current-action)
-                         (get-fv config)])))))))
+         (map (fn [f] (str (:type f) "-and-" (:str f)))))))
