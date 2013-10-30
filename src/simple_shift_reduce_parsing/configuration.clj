2064d88c3825d9c2b8321a3c6deb81d1128d6849
Yasuhisa Yoshida <syou6162@gmail.com>
2013-10-29 19:37:31 -0700

    Wordのtype hintを追加

--- src/simple_shift_reduce_parsing/configuration.clj
+++ src/simple_shift_reduce_parsing/configuration.clj
@@ -2,12 +2,15 @@
   (:refer-clojure :exclude [reduce])
   (:use simple_shift_reduce_parsing.word))
 
+(import '[simple_shift_reduce_parsing.word Word])
+
 (defrecord Configuration [stack input relations])
 
 (defn make-Configuration [input]
   (Configuration. [] input {:head-to-modifiers {} :modifier-to-head {}}))
 
-(defn add-dependency-arc [^Configuration config head-node modifier-node]
+(defn add-dependency-arc
+  [^Configuration config ^Word head-node ^Word modifier-node]
   (let [relations (:relations config)
         new-relations (-> relations
                           (update-in [:head-to-modifiers head-node]
