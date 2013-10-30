7b845edf29efb21c1cbf75ce06e9c0b6b6af54d3
Yasuhisa Yoshida <syou6162@gmail.com>
2013-10-29 19:51:20 -0700

    rootは評価から除くように変更

--- src/simple_shift_reduce_parsing/evaluation.clj
+++ src/simple_shift_reduce_parsing/evaluation.clj
@@ -1,17 +1,13 @@
 (ns simple_shift_reduce_parsing.evaluation)
 
 (defn get-dependency-accuracy [original-sentences parsed-sentences]
-  "original-sentences: flat sentences
-   parsed-sentences: sentences with hierarchical structure"
-  (let [original-sentences (rest original-sentences) ;; remove root
-        parsed-sentences (rest parsed-sentences) ;; remove root
-        pairs (map (fn [[original-sentence parsed-sentence]]
+  (let [pairs (map (fn [[original-sentence parsed-sentence]]
                      (let [num-of-correct-heads (reduce + (map (fn [[gold predict]]
                                                                  (if (= gold predict) 1.0 0.0))
                                                                (map vector
-                                                                    (map :head original-sentence)
-                                                                    (map :head parsed-sentence))))]
-                       (vector num-of-correct-heads (count original-sentence))))
+                                                                    (map :head (rest original-sentence))
+                                                                    (map :head (rest parsed-sentence)))))]
+                       (vector num-of-correct-heads (dec (count original-sentence)))))
                    (map vector original-sentences parsed-sentences))
         corrects (map first pairs)
         lengths (map second pairs)]
