cb3d776e211dd9b2cafa2e4d59e1826037fd9e0e
Yasuhisa Yoshida <syou6162@gmail.com>
2014-01-03 18:39:36 +0900

    元の文をフィールドに追加、queueにはPersistentQueueを採用

--- src/simple_shift_reduce_parsing/configuration.clj
+++ src/simple_shift_reduce_parsing/configuration.clj
@@ -4,17 +4,24 @@
 
 (import '[simple_shift_reduce_parsing.word Word])
 
-(defrecord Configuration [stack input relations])
+(defrecord Configuration
+    [^clojure.lang.PersistentVector sentence
+     ^clojure.lang.PersistentVector stack
+     ^clojure.lang.PersistentQueue input
+     ^clojure.lang.PersistentArrayMap relations])
 
 (defn make-Configuration [input]
-  (Configuration. [] input {:head-to-modifiers {} :modifier-to-head {}}))
+  (Configuration. input []
+                  (->> (mapv :idx input)
+                       (apply conj clojure.lang.PersistentQueue/EMPTY))
+                  {:head-to-modifiers {} :modifier-to-head {}}))
 
 (defn add-dependency-arc
-  [^Configuration config ^Word head-node ^Word modifier-node]
+  [^Configuration config head-idx modifier-idx]
   (let [relations (:relations config)
         new-relations (-> relations
-                          (update-in [:head-to-modifiers head-node]
-                                     (comp vec conj) modifier-node)
-                          (assoc-in [:modifier-to-head modifier-node]
-                                    head-node))]
+                          (update-in [:head-to-modifiers head-idx]
+                                     (comp vec conj) modifier-idx)
+                          (assoc-in [:modifier-to-head modifier-idx]
+                                    head-idx))]
     (assoc config :relations new-relations)))
