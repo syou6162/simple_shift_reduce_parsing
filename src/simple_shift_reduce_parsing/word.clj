4d2ef6eeb4dbafa46d7514a7acbaee918f16f0af
Yasuhisa Yoshida <syou6162@gmail.com>
2014-01-16 21:09:03 +0900

    フィールドに文字列をidに変換したものを追加

--- src/simple_shift_reduce_parsing/word.clj
+++ src/simple_shift_reduce_parsing/word.clj
@@ -1,7 +1,10 @@
 (ns simple_shift_reduce_parsing.word)
+(import Jenkins)
 
-(defrecord Word [^String surface ^String lemma
-                 ^String pos-tag ^String cpos-tag
+(defrecord Word [^String surface ^long surface-id
+                 ^String lemma ^long lemma-id
+                 ^String pos-tag ^long pos-tag-id
+                 ^String cpos-tag ^long cpos-tag-id
                  ^long idx ^long head])
 
 (defn normalize-num-expr [^String s]
@@ -9,11 +12,14 @@
     "<num>"
     s))
 
-(defn make [^String surface ^String pos-tag ^long idx ^long head]
-  (let [lemma (if (< 5 (count surface))
+(defn make [^String surface' ^String pos-tag ^long idx ^long head]
+  (let [surface (normalize-num-expr surface')
+        lemma (if (< 5 (count surface))
                 (-> surface (subs 0 5) normalize-num-expr)
                 (-> surface normalize-num-expr))
         cpos-tag (subs pos-tag 0 1)]
-    (Word. (normalize-num-expr surface) lemma
-           pos-tag cpos-tag
+    (Word. surface (Jenkins/hashString surface)
+           lemma (Jenkins/hashString lemma)
+           pos-tag (Jenkins/hashString pos-tag)
+           cpos-tag (Jenkins/hashString cpos-tag)
            idx head)))
